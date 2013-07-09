ddf59ca9c8f413aa25a2a41c1b8b7492f4d38595
Jimmy Vu <jimmyhvu@gmail.com>
2013-07-09 20:18:23 +0000

    Finish grader.js

--- grader.js
+++ grader.js
@@ -22,6 +22,7 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
@@ -65,10 +66,20 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Path to URL')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+	rest.get(program.url).on('complete', function(result){
+	  fs.writeFileSync("temp.html",result);
+	  var checkJson = checkHtmlFile("temp.html", program.checks);
+	  var outJson = JSON.stringify(checkJson, null, 4);
+	  console.log(outJson);
+	});
+    } else {
+	  var checkJson = checkHtmlFile(program.file, program.checks);
+	  var outJson = JSON.stringify(checkJson, null, 4);
+	  console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
